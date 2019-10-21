4df432938a30fc0655a47419d40d832ec02be51d
Sócrates Lopes <lopes.socrates@hotmail.com>
2019-10-21 00:49:29 -0300

    importing

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,5 @@
+from src.api.health import health_api
+from src.api.importer import importer_api
 import os
 
 from flask import Flask
@@ -9,7 +11,11 @@ def create_app():
     app.config.from_object(app_config[os.getenv('FLASK_CONFIG', default='development')])
 
     app.config['LOGSTASH'] = os.getenv('LOGSTASH')
-    app.config['ELASTICSEARCH'] = os.getenv('ELASTICSEARCH')
+    app.config['ELASTICSEARCH'] = os.getenv('ELASTICSEARCH', 'elasticsearch:9200')
+    app.config['ES_IDX'] = os.getenv('ES_IDX', 'pdvs')
+
+    app.register_blueprint(health_api)
+    app.register_blueprint(importer_api)
     app.config['APP_NAME'] = os.getenv('APP_NAME')
     app.config['URL_SERVER_PORT'] = int(os.getenv('URL_SERVER_PORT', 5000))
 
