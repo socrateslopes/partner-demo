89ad874c2466dbc872871e26b598d1d7e8f7063a
Sócrates Lopes <lopes.socrates@hotmail.com>
2019-10-27 23:29:23 -0300

    Add generic error response

--- src/application.py
+++ src/application.py
@@ -1,8 +1,19 @@
-from src.utils.flask_logger import FlaskLogger
+from werkzeug.exceptions import HTTPException, default_exceptions
+from flask import jsonify
 from src import create_app
+from src.utils.flask_logger import FlaskLogger
 
 app = create_app()
 FlaskLogger(app)
 
+@app.errorhandler(Exception)
+def handle_error(e):
+    code = 500
+    if isinstance(e, HTTPException):
+        code = e.code
+    return jsonify(error=str(e)), code
+
 if __name__ == '__main__':
+    for ex in default_exceptions:
+        app.register_error_handler(ex, handle_error)
     app.run(port=app.config['URL_SERVER_PORT'], host='0.0.0.0')
